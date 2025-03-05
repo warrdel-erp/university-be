ef11c3e6d4239ebcefe9e2948130c0611c6cf0e4
Kuldeepyadav0101 <kuldeep.yadav@warrdel.com>
2025-03-05 13:52:24 +0530

    minor change

--- services/studentService.js
+++ services/studentService.js
@@ -53,7 +53,7 @@ export async function addStudent(info, files,createdBy,universityId,roleId,acedm
     const role = 'Student';
     const registerStudentData = { studentId, email, phoneNumber, mobileNumber, scholarNumber, role,universityId,roleId };
 
-    const data = {studentId,acedmicYearId,classSectionId}
+    const data = {studentId,acedmicYearId,classSectionId,createdBy}
     console.log(`>>>>>data>>>>>>`,data)
     const result = await studentRepository.classStudentMapping(data, transaction);
     console.log(`>>>>>result>>>>>>`,result)
