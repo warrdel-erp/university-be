8251158ec182e522a5685a10ea8271c632f37769
Kuldeepyadav0101 <kuldeep.yadav@warrdel.com>
2025-06-17 21:35:14 +0530

    admin api change

--- services/timeTableCreateServices.js
+++ services/timeTableCreateServices.js
@@ -45,59 +45,6 @@ export async function deleteTimeTableCreate(TimeTableCreateId){
     return await timeTableCreateRepository.deleteTimeTableCreate(TimeTableCreateId)
 };
 
-// export async function addtimeTableMapping(data, createdBy, updatedBy) {
-    
-//     const transaction = await sequelize.transaction();
-//     let teacherSubjectData = ''
-//     try {
-        
-//         const { timeTableNameId,timeTableCreateId,timeTableCreationId,employeeId,teacherSubjectMappingId,isSameTeacher,day, period} = data
-// console.log(`>>>>>>>timeTableCreationId`,timeTableCreationId);
-
-//         // Fetch time table data
-//         const timeTableData = await getSingleTimeTableById(timeTableCreationId);
-//         console.log(`>>>>timeTableData>>>>`,timeTableData);
-        
-//         const periodLength = timeTableData[0]?.dataValues?.periodLength || 0;
-// console.log(`>>>>>>periodLength`,periodLength);
-
-//         // Fetch teacher subject data
-//         if(teacherSubjectMappingId){
-//          teacherSubjectData = await getTeacherDetailsByTeacherSubjectId(teacherSubjectMappingId);
-//          console.log(`>>>>>>>>>>>teacherSubjectData`,teacherSubjectData);
-         
-//     }
-//     const employeeIdData =  teacherSubjectData ? teacherSubjectData[0].dataValues.employeeId : employeeId;
-// console.log(`>>>>>>employeeIdData`,employeeIdData);
-
-//         // Fetch faculty load details        
-//         const faculityLoad = await getSingleFaculityLoadDetails(employeeIdData);
-//                 console.log(`>>>>>>faculityLoad`,faculityLoad);
-//         const faculityCurrentLoad = faculityLoad[0]?.dataValues?.currentLoad || 0;
-//         console.log(`>>>>>>faculityCurrentLoad`,faculityCurrentLoad);
-
-//         const currentLoad = (parseInt(faculityCurrentLoad) + periodLength) || 0;
-//         console.log(`>>>>>>currentLoad`,currentLoad);
-
-//         // Update faculty load
-//         await updateFaculityLoadByEmployeeId(employeeIdData, {currentLoad:currentLoad}, transaction );
-
-//         // // extra data 
-//         data.createdBy = createdBy;
-//         data.updatedBy = updatedBy;
-
-//         // Create new entry in time table entry
-//        const result =  await timeTableCreateRepository.addtimeTableMapping(data, transaction);
-
-//         // Commit the transaction
-//         await transaction.commit();
-//         return result
-//     } catch (error) {
-//         await transaction.rollback();
-//         throw error; 
-//     }
-// }
-
 export async function addtimeTableMapping(data, createdBy, updatedBy) {
     const transaction = await sequelize.transaction();
     let teacherSubjectData = null;
@@ -186,57 +133,10 @@ export async function addtimeTableMapping(data, createdBy, updatedBy) {
     }
 };
 
-// export async function addtimeTableMapping(data, createdBy, updatedBy) {
-//     const transaction = await sequelize.transaction();
-
-//     try {
-//         const { timeTableCreationId, teacherSubjectMappingId, employeeId, day, period } = data;
-
-//         // Fetch time table data
-//         const timeTableData = await getSingleTimeTableById(timeTableCreationId);
-//         const periodLength = timeTableData[0].dataValues.periodLength;
-
-//         // Fetch teacher subject data if teacherSubjectMappingId exists
-//         let employeeIdData = employeeId;  // Default to employeeId from data
-
-//         if (teacherSubjectMappingId) {
-//             const teacherSubjectData = await getTeacherDetailsByTeacherSubjectId(teacherSubjectMappingId);
-//             if (teacherSubjectData && teacherSubjectData.length > 0) {
-//                 employeeIdData = teacherSubjectData[0].dataValues.employeeId;
-//             }
-//         }
-
-//         // Fetch faculty load details
-//         const facultyLoad = await getSingleFaculityLoadDetails(employeeIdData);
-//         const facultyCurrentLoad = facultyLoad[0].dataValues.currentLoad || 0;
-//         const currentLoad = parseInt(facultyCurrentLoad) + periodLength;
-
-//         // Update faculty load
-//         await updateFaculityLoadByEmployeeId(employeeIdData, { currentLoad }, transaction);
-
-//         // Prepare the data to be inserted with metadata
-//         data.createdBy = createdBy;
-//         data.updatedBy = updatedBy;
-
-//         // Create a new entry in the time table mapping table
-//         const result = await timeTableCreateRepository.addtimeTableMapping(data, transaction);
-
-//         // Commit the transaction
-//         await transaction.commit();
-
-//         return result;
-//     } catch (error) {
-//         // Rollback the transaction if an error occurs
-//         await transaction.rollback();
-//         throw error;
-//     }
-// };
-
-
 export async function gettimeTableMappingDetail(universityId){
     return await timeTableCreateRepository.getTimeTableMappingDetail(universityId)
 };
 
 export async function getSingletimeTableMappingDetail(courseId,universityId){
-    return await timeTableCreateRepository.getSingletimeTableMappingDetail(courseId,universityId)
+    return await timeTableCreateRepository.getSingleTimeTableCreateDetails(courseId,universityId)
 };
