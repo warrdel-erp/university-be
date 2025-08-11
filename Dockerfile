31fa2d0b51bacb111878767680e0635503e79706
jitin <jitin.goyal@warrdel.com>
2025-08-11 10:35:06 +0530

    Update package-lock.json to remove deprecated dependencies, modify dbConfig.js for database connection settings, and add Docker commands to package.json scripts.

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:alpine
+# Set the working directory
+
+WORKDIR /app
+# Copy package.json and package-lock.json
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the rest of the application code
+COPY . . 
+
+# Expose the port the app runs on
+EXPOSE 6002
+
+# Start the application
+CMD ["node", "server.js"]
--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,16 +1,16 @@
 import 'dotenv/config';
 
 export const dbConfig = {
-    HOST: process.env.HOST || 'localhost',
-    USER: process.env.MYSQL_USERNAME || 'root',
-    PASSWORD: process.env.MYSQL_PASSWORD,
-    DB: process.env.MYSQL_DATABASE_NAME || 'university_db',
-    dialect: 'mysql',
+  HOST: process.env.HOST || 'localhost',
+  USER: process.env.MYSQL_USERNAME || 'root',
+  PASSWORD: process.env.MYSQL_PASSWORD,
+  DB: process.env.MYSQL_DATABASE_NAME || 'university_db',
+  dialect: 'mysql',
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 };
