d85f603468938c43e83ae9ca61bb75e50e081bd6
jitin <jitin.goyal@warrdel.com>
2025-08-11 10:35:06 +0530

    Update package-lock.json to remove deprecated dependencies, modify dbConfig.js for database connection settings, and add Docker commands to package.json scripts.

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,14 +1,14 @@
 export const dbConfig = {
-    HOST: process.env.HOST || 'localhost',
-    USER: process.env.MYSQL_USERNAME || 'root',
-    PASSWORD: process.env.MYSQL_PASSWORD|| 'kuldeep1',
-    DB: process.env.MYSQL_DATABASE_NAME || 'university2',
-    dialect: 'mysql',
+  HOST: process.env.HOST || "localhost",
+  USER: process.env.MYSQL_USERNAME || "root",
+  PASSWORD: process.env.MYSQL_PASSWORD || "rootroot",
+  DB: process.env.MYSQL_DATABASE_NAME || "stage_univeristy_db",
+  dialect: "mysql",
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 };
