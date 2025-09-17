5eb6027a3c553eff966c6b28c6f07b5bec837b22
Kuldeepyadav0101 <kuldeep.yadav@warrdel.com>
2025-09-17 16:22:41 +0530

    schedule_changes-1

--- repository/scheduleRepository.js
+++ repository/scheduleRepository.js
@@ -24,58 +24,15 @@ export async function addScheduleDetails(scheduleData) {
 
 export async function getScheduleDetails(universityId,acedmicYearId,instituteId,role) {
     try {
-        const Schedule = await model.ScheduleModel.findAll({
+        const Schedule = await model.scheduleModel.findAll({
             where: {
                         ...(acedmicYearId && { acedmicYearId }),
-                        ...(role === 'Head' && { instituteId })
+                        ...(role === 'Head' && { instituteId }),
+                        ...(universityId && { universityId }),
                     },
             attributes: { 
                 exclude: ["createdAt", "updatedAt", "deletedAt", "createdBy", "updatedBy"] 
             },
-            include: [
-                {
-                    model: model.instituteModel,
-                    as: 'ScheduleInstitute',  
-                    attributes: ["instituteName", "instituteCode"] ,
-                    include:[
-                        {
-                            model:model.campusModel,
-                            as:'campues',
-                            attributes: ["campusName", "campusCode"] ,
-                        }
-                    ]
-                    
-                },
-                {
-                    model: model.acedmicYearModel,
-                    as: 'ScheduleAcedmicYear',  
-                    attributes:  ["yearTitle", "startingDate", "endingDate"] 
-                },
-                {
-                    model:model.courseModel,
-                    as:'ScheduleCourse',
-                    attributes:["courseName","courseCode"]
-                },
-                // {
-                //     model:model.courseModel,
-                //     as:'ScheduleCourse',
-                //     attributes:["courseName","courseCode"]
-                // },
-                // {
-                //     model:model.classSectionModel,
-                //     as:'ScheduleClassSection',
-                //     attributes: { 
-                //         exclude: ["createdAt", "updatedAt", "deletedAt", "createdBy", "updatedBy"] 
-                //     }                
-                // },
-                {
-                    model: model.scheduleModel,
-                    as: 'ScheduleDetails',  
-                    attributes: { 
-                        exclude: ["createdAt", "updatedAt", "deletedAt", "createdBy", "updatedBy"] 
-                    }
-                },
-            ]
         });
         return Schedule;
     } catch (error) {
@@ -85,11 +42,11 @@ export async function getScheduleDetails(universityId,acedmicYearId,instituteId,
 };
 
 
-export async function getSingleScheduleDetails(ScheduleId) {
+export async function getSingleScheduleDetails(scheduleId,universityId) {
     try {
         const Schedule = await model.scheduleModel.findOne({
             attributes: { exclude: ["createdAt", "updatedAt", "deletedAt", "createdBy", "updatedBy"] },
-            where: { ScheduleId },
+            where: { scheduleId,universityId},
         });
 
         return Schedule;
@@ -99,113 +56,19 @@ export async function getSingleScheduleDetails(ScheduleId) {
     }
 }
 
-export async function deleteSchedule(ScheduleId) {
-    const deleted = await model.ScheduleModel.destroy({ where: { ScheduleId: ScheduleId } });
+export async function deleteSchedule(scheduleId) {
+    const deleted = await model.scheduleModel.destroy({ where: { scheduleId: scheduleId } });
     return deleted > 0;
 }
 
-export async function updateSchedule(ScheduleId, scheduleData) {
+export async function updateSchedule(scheduleId, scheduleData) {
     try {
-        const result = await model.ScheduleModel.update(scheduleData, {
-            where: { ScheduleId }
+        const result = await model.scheduleModel.update(scheduleData, {
+            where: { scheduleId }
         });
         return result; 
     } catch (error) {
-        console.error(`Error updating Schedule creation ${ScheduleId}:`, error);
+        console.error(`Error updating Schedule creation ${scheduleId}:`, error);
         throw error; 
-    }
-};
-
-export async function courseAllSubject(courseId) {
-    try {
-        const Schedule = await model.ScheduleModel.findAll({
-            attributes: { exclude: ["createdAt", "updatedAt", "deletedAt", "createdBy", "updatedBy"] },
-            where: { courseId },
-            include:[
-                {
-                    model:model.courseModel,
-                    as:'ScheduleCourse',
-                    attributes:["courseName","courseCode"]
-                },
-                {
-                    model: model.scheduleModel,
-                    as: 'ScheduleDetails',  
-                    attributes: { 
-                        exclude: ["createdAt", "updatedAt", "deletedAt", "createdBy", "updatedBy"] 
-                    },
-                    include:[
-                        {
-                            model:model.subjectModel,
-                            as:'ScheduleSubject',
-                            attributes: { 
-                                exclude: ["createdAt", "updatedAt", "deletedAt", "createdBy", "updatedBy"] 
-                            },
-                        }
-                    ]
-                },
-            ]
-        });
-
-        return Schedule;
-    } catch (error) {
-        console.error('Error fetching Schedule details:', error);
-        throw error;
-    }
-};
-
-export async function addScheduleUnit(scheduleData) {   
-    try {
-        const result = await model.ScheduleUnitModel.bulkCreate(scheduleData);
-        return result;
-    } catch (error) {
-        console.error("Error in add Schedule unit :", error);
-        throw error;
-    }
-};
-
-export async function ScheduleUnitGet(universityId, acedmicYearId, instituteId, role) {
-    try {
-        const excludedFields = ["createdAt", "updatedAt", "deletedAt", "createdBy", "updatedBy"];
-
-        const ScheduleUnit = await model.ScheduleUnitModel.findAll({
-            where: {
-                ...(acedmicYearId && { acedmicYearId }),
-                ...(role === 'Head' && { instituteId }),
-                ...(universityId && { universityId }),
-            },
-            attributes: { exclude: excludedFields },
-            include: [
-                {
-                    model: model.instituteModel,
-                    as: 'instituteUnit',
-                    attributes: ["instituteName", "instituteCode"],
-                },
-                {
-                    model: model.acedmicYearModel,
-                    as: 'acedmicYearUnit',
-                    attributes: ["yearTitle", "startingDate", "endingDate"],
-                },
-                {
-                    model: model.sessionModel,
-                    as: 'sessionUnit',
-                    attributes: ["sessionName"],
-                },
-                {
-                    model: model.semesterModel,
-                    as: 'semesterUnit',
-                    attributes: { exclude: excludedFields },
-                },
-                {
-                    model: model.subjectModel,
-                    as: 'subjectUnit',
-                    attributes: { exclude: excludedFields },
-                },
-            ],
-        });
-
-        return ScheduleUnit;
-    } catch (error) {
-        console.error('Error fetching Schedule unit with details:', error);
-        throw error;
     }
 };
--- services/scheduleServices.js
+++ services/scheduleServices.js
@@ -21,72 +21,16 @@ export async function getScheduleDetails(universityId,acedmicYearId,instituteId,
     return await scheduleCreationRepository.getScheduleDetails(universityId,acedmicYearId,instituteId,role);
 };
 
-export async function getSingleScheduleDetails(ScheduleId,universityId) {
-    return await scheduleCreationRepository.getSingleScheduleDetails(ScheduleId,universityId);
+export async function getSingleScheduleDetails(scheduleId,universityId) {
+    return await scheduleCreationRepository.getSingleScheduleDetails(scheduleId,universityId);
 };
 
-export async function deleteSchedule(ScheduleId) {
-    return await scheduleCreationRepository.deleteSchedule(ScheduleId);
+export async function deleteSchedule(scheduleId) {
+    return await scheduleCreationRepository.deleteSchedule(scheduleId);
 };
 
-export async function updateSchedule(ScheduleId, ScheduleData, updatedBy) {    
+export async function updateSchedule(scheduleId, ScheduleData, updatedBy) {    
 
     ScheduleData.updatedBy = updatedBy;
-    await scheduleCreationRepository.updateSchedule(ScheduleId, ScheduleData);
-};
-
-export async function courseAllSubject(courseId) {
-    return await scheduleCreationRepository.courseAllSubject(courseId);
-};
-
-export async function addScheduleUnit(data, createdBy, updatedBy, universityId, instituteId) {
-  const { acedmicYearId, semesterId, subjectId, slab,sessionId } = data;
-  const ScheduleUnits = slab.map((unit) => ({
-    universityId,
-    instituteId,
-    sessionId,
-    acedmicYearId,
-    semesterId,
-    subjectId,
-    unitNumber: unit.unitNumber,
-    name: unit.name,
-    description: unit.description,
-    contactHours: unit.contactHours,
-    createdBy,
-    updatedBy
-  }));
-
-  return await scheduleCreationRepository.addScheduleUnit(ScheduleUnits);
-};
-
-export async function ScheduleUnitGet(universityId, acedmicYearId, instituteId, role) {
-  const ScheduleUnits = await scheduleCreationRepository.ScheduleUnitGet(
-    universityId,
-    acedmicYearId,
-    instituteId,
-    role
-  );
-
-  return ScheduleUnits.map(unit => ({
-    ScheduleUnitId: unit.ScheduleUnitId,
-    universityId: unit.universityId,
-    instituteId: unit.instituteId,
-    instituteName: unit.instituteUnit?.instituteName || null,
-    instituteCode: unit.instituteUnit?.instituteCode || null,
-    acedmicYearId: unit.acedmicYearId,
-    acedmicYearTitle: unit.acedmicYearUnit?.yearTitle || null,
-    acedmicYearStart: unit.acedmicYearUnit?.startingDate || null,
-    acedmicYearEnd: unit.acedmicYearUnit?.endingDate || null,
-    semesterId: unit.semesterId,
-    name:unit.semesterUnit?.name,
-    sessionId: unit.sessionId,
-    sessionName: unit.sessionUnit?.sessionName || null,
-    subjectId: unit.subjectId,
-    subjectName: unit.subjectUnit?.subjectName || null,
-    subjectCode: unit.subjectUnit?.subjectCode || null,
-    unitNumber: unit.unitNumber,
-    name: unit.name,
-    description: unit.description,
-    contactHours: unit.contactHours
-  }));
+    await scheduleCreationRepository.updateSchedule(scheduleId, ScheduleData);
 };
