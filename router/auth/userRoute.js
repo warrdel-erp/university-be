bf87b001e11ddcb6fd840c94bb7e75019f92e3a2
Bhaskar Yogi <bhaskaryogi@Bhaskars-MacBook-Pro.local>
2026-03-03 10:29:47 +0530

    update userRoute.js

--- router/auth/userRoute.js
+++ router/auth/userRoute.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const router = Router();
 
-import { login, register, adminRegisterStudentAndEmployee, getAdminRegisterStudentAndEmployee, changePassword, changeStatus, sendLink, forgotPassword, forgotChangePassword } from "../../controllers/userController.js";
+import { login, register, adminRegisterStudentAndEmployee, getAdminRegisterStudentAndEmployee, changePassword, changeStatus, sendLink, forgotPassword, forgotChangePassword, getMyDetails } from "../../controllers/userController.js";
 import useAuth from "../../middleware/authUser.js";
 
 // for first time register
@@ -32,4 +32,5 @@ router.post("/forgotPassword", forgotPassword);
 
 router.patch("/forgotPassword", useAuth, forgotChangePassword);
 
+router.get("/myDetails", useAuth, getMyDetails);
 export default router;
