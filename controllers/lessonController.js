9367d1cebd557f3e3818dc5596f02692fe108f33
jitin <jitin.goyal@warrdel.com>
2026-03-12 19:19:47 +0530

    defaultInstituteId in lesson add

--- controllers/lessonController.js
+++ controllers/lessonController.js
@@ -1,16 +1,16 @@
-import * as lesson  from  "../services/lessonServices.js";
+import * as lesson from "../services/lessonServices.js";
 
-export async function addLesson(req, res) {        
-    const {name,subjectId,acedmicYearId,sessionId} = req.body
+export async function addLesson(req, res) {
+    const { name, subjectId, acedmicYearId, sessionId } = req.body
     const createdBy = req.user.userId;
     const updatedBy = req.user.userId;
     const universityId = req.user.universityId;
-    const instituteId = req.user.instituteId;
+    const instituteId = req.user.defaultInstituteId;
     try {
-        if(!(name && subjectId  && acedmicYearId && sessionId)){
-           return res.status(400).send('name,subjectId,acedmicYearId and sessionId is required')
+        if (!(name && subjectId && acedmicYearId && sessionId)) {
+            return res.status(400).send('name,subjectId,acedmicYearId and sessionId is required')
         }
-        const lessonData = await lesson.addLesson(req.body,createdBy,updatedBy,universityId,instituteId);
+        const lessonData = await lesson.addLesson(req.body, createdBy, updatedBy, universityId, instituteId);
         res.status(201).json({ message: "Data added successfully", lessonData });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -20,10 +20,10 @@ export async function addLesson(req, res) {
 export async function getAllLesson(req, res) {
     const universityId = req.user.universityId;
     const instituteId = req.user.instituteId;
-    const role = req.user.role;    
-    const {acedmicYearId} = req.query
+    const role = req.user.role;
+    const { acedmicYearId } = req.query
     try {
-        const Lessons = await lesson.getLessonDetails(universityId,instituteId,role,acedmicYearId);
+        const Lessons = await lesson.getLessonDetails(universityId, instituteId, role, acedmicYearId);
         res.status(200).json(Lessons);
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -45,34 +45,34 @@ export async function getSingleLessonDetails(req, res) {
     }
 };
 
-export async function addTopice(req, res) {        
-    const {name,lessonId} = req.body
+export async function addTopice(req, res) {
+    const { name, lessonId } = req.body
     const createdBy = req.user.userId;
     const updatedBy = req.user.userId;
     const universityId = req.user.universityId;
     const instituteId = req.user.instituteId;
     try {
-        if(!(name && lessonId)){
-           return res.status(400).send('name and lessionId is required')
+        if (!(name && lessonId)) {
+            return res.status(400).send('name and lessionId is required')
         }
-        const lessonData = await lesson.addTopice(req.body,createdBy,updatedBy,universityId,instituteId);
+        const lessonData = await lesson.addTopice(req.body, createdBy, updatedBy, universityId, instituteId);
         res.status(201).json({ message: "Data added successfully", lessonData });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 };
 
-export async function addMapping(req, res) {        
-    const {topicId,timeTableMappingId,date} = req.body
+export async function addMapping(req, res) {
+    const { topicId, timeTableMappingId, date } = req.body
     const createdBy = req.user.userId;
     const updatedBy = req.user.userId;
     const universityId = req.user.universityId;
     const instituteId = req.user.instituteId;
     try {
-        if(!(topicId && timeTableMappingId && date)){
-           return res.status(400).send('topiceId,timeTableMappingId and date is required')
+        if (!(topicId && timeTableMappingId && date)) {
+            return res.status(400).send('topiceId,timeTableMappingId and date is required')
         }
-        const lessonData = await lesson.addMapping(req.body,createdBy,updatedBy,universityId,instituteId);
+        const lessonData = await lesson.addMapping(req.body, createdBy, updatedBy, universityId, instituteId);
         res.status(201).json({ message: "Data added successfully", lessonData });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -82,10 +82,10 @@ export async function addMapping(req, res) {
 export async function getMapping(req, res) {
     const universityId = req.user.universityId;
     const instituteId = req.user.instituteId;
-    const role = req.user.role;    
-    const {acedmicYearId} = req.query
+    const role = req.user.role;
+    const { acedmicYearId } = req.query
     try {
-        const Lessons = await lesson.getMapping(universityId,instituteId,role,acedmicYearId);
+        const Lessons = await lesson.getMapping(universityId, instituteId, role, acedmicYearId);
         res.status(200).json(Lessons);
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -93,12 +93,12 @@ export async function getMapping(req, res) {
 };
 
 export async function updateMapping(req, res) {
-    const {completeDate,lessonMappingId} = req.body
-    if(!(completeDate && lessonMappingId)){
-           return res.status(400).send('completeDate and lessionMappingId is required')
-        }
+    const { completeDate, lessonMappingId } = req.body
+    if (!(completeDate && lessonMappingId)) {
+        return res.status(400).send('completeDate and lessionMappingId is required')
+    }
     try {
-        const Lessons = await lesson.updateMapping(completeDate,lessonMappingId);
+        const Lessons = await lesson.updateMapping(completeDate, lessonMappingId);
         res.status(200).json(Lessons);
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -133,10 +133,10 @@ export async function deleteMapping(req, res) {
 };
 
 export async function getEmployeeSubjectAndLesson(req, res) {
-    const role = req.user.role;    
-    const {acedmicYearId,employeeId,courseId,sessionId} = req.query
+    const role = req.user.role;
+    const { acedmicYearId, employeeId, courseId, sessionId } = req.query
     try {
-        const Lessons = await lesson.getEmployeeSubjectAndLesson(acedmicYearId,employeeId,courseId,sessionId);
+        const Lessons = await lesson.getEmployeeSubjectAndLesson(acedmicYearId, employeeId, courseId, sessionId);
         res.status(200).json(Lessons);
     } catch (error) {
         res.status(500).json({ error: error.message });
