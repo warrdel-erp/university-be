02d8e3f86a9cefb39933648af6b69a160fcdd781
jitin <jitin.goyal@warrdel.com>
2026-04-01 13:18:24 +0530

    revert

--- models/index.js
+++ models/index.js
@@ -231,7 +231,9 @@ specializationModel.hasMany(semesterModel, { foreignKey: "specialization_id", as
 classSectionModel.belongsTo(specializationModel, { foreignKey: "specialization_id", as: "specializationSectionAdd" });
 specializationModel.hasMany(classSectionModel, { foreignKey: "specialization_id", as: "specializationSectionAdd" });
 
-
+// class subject mapper join to class section
+classSubjectMapperModel.belongsTo(semesterModel, { foreignKey: "semester_id", as: "semestermapping" });
+semesterModel.hasMany(classSubjectMapperModel, { foreignKey: "semester_id", as: "semestermapping" });
 
 // class section join to subject
 classSubjectMapperModel.belongsTo(subjectModel, { foreignKey: "subject_id", as: "subjects" });
@@ -409,7 +411,8 @@ teacherSubjectMappingModel.belongsTo(classSubjectMapperModel, {
   as: "employeeSubject",
 });
 
-
+semesterModel.hasMany(classSubjectMapperModel, { foreignKey: "semesterId", as: "employeeClassSection" });
+classSubjectMapperModel.belongsTo(semesterModel, { foreignKey: "semesterId", as: "employeeClassSection" });
 
 // teacher section mapping
 employeeModel.hasMany(teacherSectionMappingModel, { foreignKey: "employee_id", as: "employeeData" });
