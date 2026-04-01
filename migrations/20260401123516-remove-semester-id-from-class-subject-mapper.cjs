d99988999c34205446cc5b505c38639df33fd3a9
jitin <jitin.goyal@warrdel.com>
2026-04-01 13:20:01 +0530

    undo

--- migrations/20260401123516-remove-semester-id-from-class-subject-mapper.cjs
+++ migrations/20260401123516-remove-semester-id-from-class-subject-mapper.cjs
@@ -9,7 +9,7 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     await queryInterface.addColumn('class_subject_mapper', 'semester_id', {
       type: Sequelize.INTEGER,
-      allowNull: false,
+      allowNull: true,
       references: {
         model: 'semester',
         key: 'semester_id'
