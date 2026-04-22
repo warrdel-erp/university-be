2e8c2e3b2bce249993996da41ddd022a1420fbe3
jitin <jitin.goyal@warrdel.com>
2026-04-22 17:29:27 +0530

    migration chenge

--- migrations/20260422153500-make-classroom-exam-capacity-nullable.cjs
+++ migrations/20260422153500-make-classroom-exam-capacity-nullable.cjs
@@ -5,15 +5,33 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     const transaction = await queryInterface.sequelize.transaction();
     try {
-      await queryInterface.changeColumn('class_room_section', 'exam_capacity', {
-        type: Sequelize.INTEGER,
-        allowNull: true
-      }, { transaction });
+      const tableInfo = await queryInterface.describeTable('class_room_section', { transaction });
 
-      await queryInterface.changeColumn('class_room_section', 'exam_capacity_columns', {
-        type: Sequelize.INTEGER,
-        allowNull: true
-      }, { transaction });
+      if (tableInfo.exam_capacity) {
+        await queryInterface.changeColumn('class_room_section', 'exam_capacity', {
+          type: Sequelize.INTEGER,
+          allowNull: true
+        }, { transaction });
+      } else {
+        await queryInterface.addColumn('class_room_section', 'exam_capacity', {
+          type: Sequelize.INTEGER,
+          allowNull: true,
+          after: 'capacity'
+        }, { transaction });
+      }
+
+      if (tableInfo.exam_capacity_columns) {
+        await queryInterface.changeColumn('class_room_section', 'exam_capacity_columns', {
+          type: Sequelize.INTEGER,
+          allowNull: true
+        }, { transaction });
+      } else {
+        await queryInterface.addColumn('class_room_section', 'exam_capacity_columns', {
+          type: Sequelize.INTEGER,
+          allowNull: true,
+          after: 'exam_capacity'
+        }, { transaction });
+      }
 
       await transaction.commit();
     } catch (error) {
@@ -25,17 +43,23 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     const transaction = await queryInterface.sequelize.transaction();
     try {
-      await queryInterface.changeColumn('class_room_section', 'exam_capacity', {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        defaultValue: 0
-      }, { transaction });
+      const tableInfo = await queryInterface.describeTable('class_room_section', { transaction });
 
-      await queryInterface.changeColumn('class_room_section', 'exam_capacity_columns', {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        defaultValue: 1
-      }, { transaction });
+      if (tableInfo.exam_capacity) {
+        await queryInterface.changeColumn('class_room_section', 'exam_capacity', {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          defaultValue: 0
+        }, { transaction });
+      }
+
+      if (tableInfo.exam_capacity_columns) {
+        await queryInterface.changeColumn('class_room_section', 'exam_capacity_columns', {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          defaultValue: 1
+        }, { transaction });
+      }
 
       await transaction.commit();
     } catch (error) {
