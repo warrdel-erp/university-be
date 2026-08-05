d18c213f04b5098c8fa528e416d353e2c49e7168
Gaurav Singh Solanki <gauravpanahera@gmail.com>
2026-08-05 16:20:51 +0530

    Update examinationSessionSlotRepository.js

--- repository/examinationSessionSlotRepository.js
+++ repository/examinationSessionSlotRepository.js
@@ -1,5 +1,6 @@
 import * as model from "../models/index.js";
 import { scoped } from "../utility/scoped.js";
+import { Op } from "sequelize";
 
 export async function getMaxSlotNumber(examinationSessionId, options = {}) {
   const highestSlot = await scoped(model.examinationSessionSlotModel).findOne({
@@ -19,12 +20,71 @@ export async function createExaminationSessionSlot(slotData, options = {}) {
   });
 }
 
-export async function getExaminationSessionSlots(examinationSessionId, options = {}) {
-  return await scoped(model.examinationSessionSlotModel).findAll({
-    where: { examinationSessionId: Number(examinationSessionId) },
+export async function getExaminationSessionSlots(
+  examinationSessionId,
+  options = {}
+) {
+  const slots = await scoped(model.examinationSessionSlotModel).findAll({
+    where: {
+      examinationSessionId: Number(examinationSessionId),
+    },
     order: [["slotNumber", "ASC"]],
+    raw: true,
     transaction: options.transaction,
   });
+
+  if (!slots.length) {
+    return [];
+  }
+
+  const slotIds = [];
+
+  for (const slot of slots) {
+    slotIds.push(slot.examinationSessionSlotId);
+  }
+
+  const schedules = await scoped(model.examScheduleModel).findAll({
+    where: {
+      examinationSessionSlotId: {
+        [Op.in]: slotIds,
+      },
+    },
+    include: [
+      {
+        model: model.subjectModel,
+        as: "subjectSchedule",
+        attributes: ["subjectId", "subjectName", "subjectCode"],
+      },
+    ],
+    order: [
+      ["examDate", "ASC"],
+      ["examTime", "ASC"],
+    ],
+    transaction: options.transaction,
+  });
+
+  const scheduleMap = new Map();
+
+  for (const schedule of schedules) {
+    const item = schedule.get({ plain: true });
+
+    if (!scheduleMap.has(item.examinationSessionSlotId)) {
+      scheduleMap.set(item.examinationSessionSlotId, []);
+    }
+
+    scheduleMap.get(item.examinationSessionSlotId).push(item);
+  }
+
+  const result = [];
+
+  for (const slot of slots) {
+    result.push({
+      ...slot,
+      schedules: scheduleMap.get(slot.examinationSessionSlotId) || [],
+    });
+  }
+
+  return result;
 }
 
 export async function getExaminationSessionSlotById(examinationSessionSlotId, options = {}) {
