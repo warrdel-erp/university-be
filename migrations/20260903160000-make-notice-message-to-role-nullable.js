f58f122d7ee406bfb57a77830afc769e2240a000
Gaurav Singh Solanki <gauravpanahera@gmail.com>
2026-09-04 21:01:51 +0530

    .

--- migrations/20260903160000-make-notice-message-to-role-nullable.js
+++ migrations/20260903160000-make-notice-message-to-role-nullable.js
@@ -2,25 +2,61 @@
 
 /** @type {import('sequelize-cli').Migration} */
 export async function up(queryInterface, Sequelize) {
-  await queryInterface.changeColumn("notice", "message_to", {
-    type: Sequelize.JSON,
-    allowNull: true,
-  });
+  const transaction = await queryInterface.sequelize.transaction();
+  try {
+    await queryInterface.changeColumn(
+      "notice",
+      "message_to",
+      {
+        type: Sequelize.JSON,
+        allowNull: true,
+      },
+      { transaction },
+    );
 
-  await queryInterface.changeColumn("notice", "role", {
-    type: Sequelize.STRING,
-    allowNull: true,
-  });
+    await queryInterface.changeColumn(
+      "notice",
+      "role",
+      {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      { transaction },
+    );
+
+    await transaction.commit();
+  } catch (error) {
+    await transaction.rollback();
+    throw error;
+  }
 }
 
 export async function down(queryInterface, Sequelize) {
-  await queryInterface.changeColumn("notice", "message_to", {
-    type: Sequelize.JSON,
-    allowNull: false,
-  });
+  const transaction = await queryInterface.sequelize.transaction();
+  try {
+    await queryInterface.changeColumn(
+      "notice",
+      "message_to",
+      {
+        type: Sequelize.JSON,
+        allowNull: false,
+      },
+      { transaction },
+    );
 
-  await queryInterface.changeColumn("notice", "role", {
-    type: Sequelize.STRING,
-    allowNull: false,
-  });
+    await queryInterface.changeColumn(
+      "notice",
+      "role",
+      {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      { transaction },
+    );
+
+    await transaction.commit();
+  } catch (error) {
+    await transaction.rollback();
+    throw error;
+  }
 }
